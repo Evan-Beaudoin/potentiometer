7bcb083ccd74c1815ee03b32c9826642007748c8
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-10-06 19:37:51 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+// Created by: Evan
+// Created on: Oct.2020
+// 
+// This program uses the potentiometer and displays the resistance value
 let Potentiometer = 0
 basic.showIcon(IconNames.Yes)
 basic.pause(200)
